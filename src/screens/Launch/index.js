53e0078c440adccb4d3758b062210699a299d39d
Test101Bug <zookeroro@gmail.com>
2020-09-08 14:39:07 +0700

    Update

--- src/screens/Launch/index.js
+++ src/screens/Launch/index.js
@@ -1,13 +1,48 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Image, StyleSheet } from "react-native";
+import images from "../../util/refImage";
 import { SafeAreaView } from "react-native-safe-area-context";
+import LinearGradient from "react-native-linear-gradient";
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from "react-native-responsive-screen";
 
 export default () => {
   return (
-    <View style={{ backgroundColor: "red", flex: 1 }}>
-      <SafeAreaView>
-        <Text>asdf</Text>
+    <LinearGradient
+      colors={["#0C4DA1", "#528AD1"]}
+      style={styles.linearGradient}
+    >
+      <SafeAreaView style={styles.container}>
+        <Image
+          style={styles.logoText}
+          source={images.samsung_text_white}
+        ></Image>
+        <Image
+          style={styles.logoImage}
+          source={images.samsung_logo_white}
+        ></Image>
       </SafeAreaView>
-    </View>
+    </LinearGradient>
   );
 };
+
+const styles = StyleSheet.create({
+  linearGradient: {
+    flex: 1,
+  },
+  logoText: {
+    width: wp("30"),
+    height: hp("10"),
+    resizeMode: "stretch",
+  },
+  logoImage: {
+    width: wp("120"),
+    height: hp("32"),
+    resizeMode: "stretch",
+    alignSelf:'center',
+    marginTop:hp("5")
+  },
+  container: { flex: 1 },
+});
---
+++ src/util/refImage.js
@@ -0,0 +1,6 @@
+export default {
+  samsung_logo: require("../../assets/images/samsung_logo.png"),
+  samsung_text: require("../../assets/images/samsung_text.png"),
+  samsung_logo_white: require("../../assets/images/samsung_logo_white.png"),
+  samsung_text_white: require("../../assets/images/samsung_text_white.png"),
+};
